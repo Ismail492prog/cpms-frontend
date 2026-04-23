0683ab70f6f3a5b237428e906f7491f583665e3c
Juma Kiminyu <i48462003@gmail.com>
2026-04-23 15:06:59 +0300

    Hardcode backend URL for production

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://cpms-backend-production.up.railway.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
